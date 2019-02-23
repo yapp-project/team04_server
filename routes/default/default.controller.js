65cce01d5d80744835c666af2ff8564e3cfe181b
ridickle7 <ridickle7@gmail.com>
2019-02-24 03:15:38 +0900

    first commit

--- routes/default/default.controller.js
+++ routes/default/default.controller.js
@@ -1,5 +1,4 @@
 let mysql = require('../../config/database/con');
-let jsonDB = require('../../config/database/jsonDB');
 let model = require('../_model/model');
 let dir = require('../../public/dir');
 
@@ -13,19 +12,4 @@ exports.post = (req, res) => {
   res.json({
     msg: 'post / connection ok'
   });
-};
-
-exports.tip = (req, res) => {
-  let honeyTip = jsonDB.goolTip;
-  let type = req.query.type;
-
-  if (type == 'nose')
-    res.json(model.default('', honeyTip,
-      '꿀팁 전송 완료',
-      model.response(200, dir.get(200))));
-  else {
-    res.json(model.default('', honeyTip,
-      '해당 타입에 맞는 데이터가 없습니다.',
-      model.response(206, dir.get(206))));
-  }
 };
